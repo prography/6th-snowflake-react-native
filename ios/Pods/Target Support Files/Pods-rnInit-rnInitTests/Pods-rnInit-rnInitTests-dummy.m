654a4a54d7bec762f79e3ecd6c940d44e601203f
zwonH <a.circle.one@gmail.com>
2020-04-03 17:10:10 +0900

    다시

---
+++ ios/Pods/Target Support Files/Pods-rnInit-rnInitTests/Pods-rnInit-rnInitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnInit_rnInitTests : NSObject
+@end
+@implementation PodsDummy_Pods_rnInit_rnInitTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-rnInit-tvOSTests/Pods-rnInit-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rnInit_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_rnInit_tvOSTests
+@end
