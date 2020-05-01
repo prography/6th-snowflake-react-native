1366e25e52e2edd8f2f0e61d31785f2aec55d6a2
daheeahn <deg9810@gmailcom>
2020-05-01 17:29:47 +0900

    Chore: text hi 추가

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import { Text } from "react-native";
 
 const App = () => {
-  return (
-    <>
-      
-    </>
-  );
+  return <Text>hi</Text>;
 };
 
 export default App;
