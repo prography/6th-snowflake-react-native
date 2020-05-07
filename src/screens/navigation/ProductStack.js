ff52e9a74f4e6e2991be2cc7bdcaf333bc246bf6
Sangwon <jackoss@naver.com>
2020-05-07 14:54:08 +0900

    스크린 폴더들

--- src/screens/navigation/ProductStack.js
+++ src/screens/navigation/ProductStack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import Product from "../Product";
+import Product from "../product/Product";
 
 const Stack = createStackNavigator();
 
