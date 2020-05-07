2d7e7cdd5ab4765840124a3a48d4a9c0c6692efe
Sangwon <jackoss@naver.com>
2020-05-07 17:31:41 +0900

    Product 완료

--- src/screens/Product/Review.tsx
+++ src/screens/Product/Review.tsx
@@ -9,7 +9,7 @@ const Container = styled.SafeAreaView`
     flex: 1;
 `;
 
-const Title = styled.View`
+const TitleContainer = styled.View`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
@@ -19,8 +19,25 @@ const Title = styled.View`
 
 const TitleText = styled.Text`
     justify-content: center;
+    font-size: 20px;
     font-weight: bold;
 `;
+
+const ProductContainer = styled.View`
+    border-bottom-width: 1px;
+`;
+
+const ImageShadow = styled.View`
+    shadowColor: black;
+    shadowOffset: {
+        width: 0,
+        height: 12,
+    };
+    shadowOpacity: 0.58;
+    shadowRadius: 16.00;
+
+    elevation: 24;
+`
 
 const ImageContainer = styled.View`
     align-items: center;
@@ -34,7 +51,6 @@ const ProductInfoContainer = styled.View`
     align-items: center;
     height: ${sHeight/5}px;
     width: ${sWidth}px;
-    background-color: red;
 `;
 
 const BrandName = styled.Text`
@@ -45,30 +61,81 @@ const BrandName = styled.Text`
 const ProductName = styled.Text`
     font-size: 22px;
     font-weight: bold;
-    margin-top: 8px;
+    margin-top: 5px;
+`;
+
+const TotalScore = styled.Text`
+    font-size: 30px;
+    font-weight: bold;
+    margin-top: 13px;
+`;
+
+const ScoreContainer = styled.View`
+    width: ${sWidth}px;
+    margin-top: 10px;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+const ScoreInfo = styled.View`
+    margin-left: ${sWidth/10}px;
+    margin-right: ${sWidth/10}px;
+    align-items: center;
+`;
+
+const ScoreTitle = styled.Text`
+    font-size: 16px;
+`;
+
+const Score = styled.Text`
+    font-size: 16px;
+    margin-top: 7px;
 `;
 
 export default () => {
     return (
         <Container>
-            <Title>
-                <Image 
-                    source={require('../../img/img1.jpeg')}
-                    style={{width: sWidth/10, height: sHeight/20}}/>
-                <TitleText>리뷰</TitleText>
-                <Image 
-                    source={require('../../img/img1.jpeg')}
-                    style={{width: sWidth/10, height: sHeight/20}}/>
-            </Title>
-            <ImageContainer>
-                <Image 
+            <TitleContainer>
+                <ImageShadow>
+                    <Image 
                         source={require('../../img/img1.jpeg')}
-                        style={{width: sWidth/2, height: sWidth/2, borderRadius: sWidth/4}}/>
-            </ImageContainer>
-            <ProductInfoContainer>
-                <BrandName>듀렉스</BrandName>
-                <ProductName>필 울트라씬</ProductName>
-            </ProductInfoContainer>
+                        style={{width: sWidth/10, height: sHeight/20}}/>
+                </ImageShadow>
+                <TitleText>리뷰</TitleText>
+                <ImageShadow>
+                    <Image 
+                        source={require('../../img/img1.jpeg')}
+                        style={{width: sWidth/10, height: sHeight/20}}/>
+                </ImageShadow>
+            </TitleContainer>
+            <ProductContainer>
+                <ImageContainer>
+                    <ImageShadow>
+                        <Image 
+                                source={require('../../img/img1.jpeg')}
+                                style={{width: sWidth/2, height: sWidth/2, borderRadius: sWidth/4}}/>
+                    </ImageShadow>
+                </ImageContainer>
+                <ProductInfoContainer>
+                    <BrandName>듀렉스</BrandName>
+                    <ProductName>필 울트라씬</ProductName>
+                    <TotalScore>4.71</TotalScore>
+                    <ScoreContainer>
+                        <ScoreInfo>
+                            <ScoreTitle>얇기</ScoreTitle>
+                            <Score>⭐x5</Score>
+                        </ScoreInfo>
+                        <ScoreInfo>
+                            <ScoreTitle>얇기</ScoreTitle>
+                            <Score>⭐x5</Score>
+                        </ScoreInfo>
+                        <ScoreInfo>
+                            <ScoreTitle>얇기</ScoreTitle>
+                            <Score>⭐x5</Score>
+                        </ScoreInfo>
+                    </ScoreContainer>
+                </ProductInfoContainer>
+            </ProductContainer>
             
         </Container>
     
