f0633ed67f8b90d420b280efb30ff503b5e4d201
Sangwon <jackoss@naver.com>
2020-05-10 16:54:31 +0900

    chore: Review_ProductScore

--- src/components/review/ReviewProduct.tsx
+++ src/components/review/ReviewProduct.tsx
@@ -9,7 +9,6 @@ const Container = styled.View`
     width: ${device.width}px
     background-color: white;
     border-radius: 50px;
-    position: absolute;
     shadow-color: "#000"
     shadow-offset: {
         width: 10,
@@ -21,37 +20,37 @@ const Container = styled.View`
 `;
 
 const BackContainer = styled.View`
-    height: ${device.width * 0.5}px;
+    height: ${device.width * 0.6}px;
     width: ${device.width}px;
     background-color: white;
-    position: absolute;
     flex-direction: row;
 `;
 
 const ProductInfoContainer = styled.View`
     height: ${device.width * 0.45}px;
-    width: ${device.width * 0.4}px;
-    padding-top: ${device.width * 0.05}px;
+    width: ${device.width * 0.38}px;
+    margin-top: ${device.width * 0.05}px;
     margin-left: ${device.width * 0.1}px;
 `;
 
 const RankNumText = styled.Text`
     color: ${color.grayDark};
     font-weight: 900;
+    font-size: ${device.px * 13}px;
 `;
 
 const BrandText = styled.Text`
     color: ${color.grayLight};
     font-weight: 900;
     margin-top: ${device.width * 0.03}px;
-    font-size: 18px;
+    font-size: ${device.px * 16}px;
 `;
 
 const NameText = styled.Text`
     color: ${color.grayDark}
     font-weight: 900;
     margin-top: ${device.width * 0.02}px;
-    font-size: 25px;
+    font-size: ${device.px * 23}px;
 `;
 
 const ThreeInfoContainer = styled.View`
@@ -63,9 +62,52 @@ const InfoText = styled.Text`
     margin-top: ${device.width * 0.01}px;
 `;
 
+const TotalScoreText = styled.Text`
+    color: ${color.grayDark};
+    margin-top: ${device.width * 0.02}px;
+    font-weight: 700;
+    font-size: ${device.px * 42}px;
+`;
+
+const ImageContainer = styled.View`
+    margin-top: ${device.width * 0.05}px;
+`;
+
+const Image = styled.Image`
+    height: ${device.width * 0.52}px;
+    width: ${device.width * 0.5}px;
+    resize-mode: contain;
+`;
+
+const TrioScoreContainer = styled.View`
+    height: ${device.width * 0.1}px;
+    width: ${device.width}px;
+    margin-top: ${device.width * 0.02}px;
+    flex-direction: row;
+    justify-content: space-around;
+`;
+
+const EachScoreContainer = styled.View`
+    align-items: center;
+`;
+
+const ScoreTitle = styled.Text`
+    margin-top: ${device.width * 0.01}px;
+    color: ${color.grayDark};
+    font-weight: 700;
+    font-size: ${device.px * 15}px;
+`;
+
+const Score = styled.Text`
+    color: ${color.grayDark};
+    margin-top: ${device.width * 0.01}px;
+    font-weight: 700;
+    font-size: ${device.px * 13}px;
+`;
+
+
 const ReviewProduct = (props) => {
     return (
-        
         <Container>
             <BackContainer>
                 <ProductInfoContainer>
@@ -83,8 +125,28 @@ const ReviewProduct = (props) => {
                         <InfoText>폭 {props.productInfo.width}mm</InfoText>
                         <InfoText>두께 {props.productInfo.thickness}mm</InfoText>
                     </ThreeInfoContainer>
+                    <TotalScoreText>
+                        {props.score.totalScore}
+                    </TotalScoreText>
                 </ProductInfoContainer>
+                <ImageContainer>
+                    <Image source={require('../../img/ultraThin.png')}/>
+                </ImageContainer>
             </BackContainer>
+            <TrioScoreContainer>
+                <EachScoreContainer>
+                    <ScoreTitle>얇기</ScoreTitle>
+                    <Score>{props.score.thinScore}</Score>
+                </EachScoreContainer>
+                <EachScoreContainer>
+                    <ScoreTitle>내구성</ScoreTitle>
+                    <Score>{props.score.durableScore}</Score>
+                </EachScoreContainer>
+                <EachScoreContainer>
+                    <ScoreTitle>윤활제</ScoreTitle>
+                    <Score>{props.score.slushScore}</Score>
+                </EachScoreContainer>
+            </TrioScoreContainer>
         </Container>
     );
 };
--- src/containers/product/ProductInfo.tsx
+++ src/containers/product/ProductInfo.tsx
@@ -4,7 +4,7 @@ import ReviewProduct from '../../components/review/ReviewProduct';
 const ProductInfo = () => {
     const rankInfo = {style: '초박형', rank : 3};//mutable
     const productInfo = {brand: '듀렉스', name: '필 울트라씬', length: '185±20', width: '49±2', thickness: '0.05'};//immutable
-    const score = {totalScore: 4.71, thinScore: 5, durableScore: 4.3, slushScore: 4};//mutable
+    const score = {totalScore: 4.71, thinScore: '★★★★★', durableScore: '★★★☆☆', slushScore: '★★★★☆'};//mutable
 
     return(
         <>
