f32aacc9453f60e6557d22fa880381f742c993e3
Sangwon <jackoss@naver.com>
2020-05-11 01:15:25 +0900

    chore: Review Screen

--- src/containers/product_review/Reviews.tsx
+++ src/containers/product_review/Reviews.tsx
@@ -1,2 +1,18 @@
 import React from 'react';
+import Review from '../../components/review/Review';
 
+const Reviews = () => {
+    const reviewList = [
+        {uri: 'https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg', score: 3.5, age: '20', filter: '남녀', text: '울트라 저의 최애입니다. 근데 이런점은 별로에요. 참고하세요~ 뭐가 좋고 뭐가 안좋았어요.. ', date: '2020.05.09', like: '1'},
+        {uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg', score: 2.5, age: '30', filter: '남녀', text: '최악....!!! 쓰지마세요ㅠㅠ 비싸기만해요ㅠㅠㅠ', date: '2020.05.08', like: '3'}
+    ];
+    return(
+        <>
+            {reviewList.map((review) => {
+                return <Review review={review}/>;
+            })}
+        </>
+    );
+};
+
+export default Reviews;
--- src/screens/Product/Review.tsx
+++ src/screens/Product/Review.tsx
@@ -3,15 +3,17 @@ import { ScrollView, Text } from 'react-native';
 import styled from 'styled-components';
 import { device } from '../../utils/constant';
 import ProductInfo from '../../containers/product_review/ProductInfo';
-import ReviewFilterContainer from '../../components/review/ReviewFilterContainer';
-import Margin from '../../components/universal/Margin';
 import ReviewFilter from '../../containers/product_review/ReviewFilter';
+import Reviews from '../../containers/product_review/Reviews';
 
 const Review = () => {
     return (
         <>
             <ProductInfo/>
             <ReviewFilter/>
+            <ScrollView>
+                <Reviews/>
+            </ScrollView>
         </>
     )
 }
