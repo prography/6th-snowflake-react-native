fcdf78c72a3cbf796535e1e33dcd1d3a80521a42
Sangwon <jackoss@naver.com>
2020-05-12 01:17:32 +0900

    chore: review_score

--- src/screens/Product/ReviewWriting.tsx
+++ src/screens/Product/ReviewWriting.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native'
 import ReviewProduct from '../../containers/product_review_writing/ReviewProduct';
+import Score from '../../containers/product_review_writing/Score';
 
 const ReviewWriting = () => {
     return (
-        <ReviewProduct/>
+        <>
+            <ReviewProduct/>
+            <Score/>
+        </>
     );
 };
 
