9a835125f101608e8a78f5272f1e47e45a19622b
Sangwon <jackoss@naver.com>
2020-05-15 18:13:44 +0900

    chore: star rating

---
+++ src/components/universal/StarRating.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import StarRating from 'react-native-star-rating';
+import { color , device } from '../../utils/constant';
+import styled from 'styled-components';
+
+const StarRatingView = styled.View`
+
+`;
+
+const GeneralStarExample = () => {
+    const [starCount, onStarRatingPress] = useState(4);
+
+    return (
+        <StarRatingView>
+        <StarRating
+            disabled={false}
+            emptyStar={'ios-star-outline'}
+            fullStar={'ios-star'}
+            halfStar={'ios-star-half'}
+            iconSet={'Ionicons'}
+            maxStars={5}
+            starSize= {25}
+            rating={starCount}
+            selectedStar={(rating) => onStarRatingPress(rating)}
+            fullStarColor={color.mainDark}
+        />
+        </StarRatingView>
+      );
+}
+
+export default GeneralStarExample
