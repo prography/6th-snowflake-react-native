85c662542c0fc7bc8fd7bfdbe937b7ebd03c82b4
Sangwon <jackoss@naver.com>
2020-05-21 23:01:48 +0900

    chore : position

--- src/screens/Product/Review.tsx
+++ src/screens/Product/Review.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, View, Text, Animated } from 'react-native';
 import styled from 'styled-components';
 import { device } from '../../utils/constant';
 import ProductInfo from '../../containers/product_review/ProductInfo';
 import ReviewFilter from '../../containers/product_review/ReviewFilter';
 import Reviews from '../../containers/product_review/Reviews';
 
+HEADER_MAX_HEIGHT = device.width * 0.8
+HEADER_MIN_HEIGHT = device.width * 0.3
+PROFILE_IMAGE_MAX_HEIGHT = device.width * 0.4
+PROFILE_IMAGE_MIN_HEIGHT = device.width * 0.2
+
 const Review = () => {
     return (
         <>
-            <ProductInfo/>
-            <ReviewFilter/>
+            <View style={{position:'absolute'}}>
+                <ProductInfo/>
+                <ReviewFilter/>
+            </View>
             <ScrollView>
-                <Reviews/>
+                <View style={{
+                    overflow:'hidden',
+                    marginTop: device.width * 0.92
+                    }}>
+                    <Reviews/>
+                </View>
             </ScrollView>
         </>
     )
