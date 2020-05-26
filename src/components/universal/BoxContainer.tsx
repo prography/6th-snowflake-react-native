a1df8af064a89e616d0acfe3316c97cbee49df16
zwonH <a.circle.one@gmail.com>
2020-05-26 16:41:29 +0900

    chore: header 삭제

--- src/components/universal/BoxContainer.tsx
+++ src/components/universal/BoxContainer.tsx
@@ -7,15 +7,8 @@ const Container = styled.View`
   height: ${device.width / 2}px;
   width: ${device.width * 0.9}px;
   background-color: white;
-  shadow-color: "#000";
-  shadow-offset: {
-      width: 10,
-      height: 10,
-  };
-  shadow-opacity: 0.3;
-  shadow-radius: 10;
-  elevation: 10;
-  border-radius:${device.width / 10}px;
+
+  border-radius: ${device.width / 10}px;
 `;
 
 const BoxContainer = () => {
--- src/navigation/JoinStack.tsx
+++ src/navigation/JoinStack.tsx
@@ -6,7 +6,11 @@ const Stack = createStackNavigator();
 
 export default () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name='GenderColor' component={GenderColor} />
     </Stack.Navigator>
   );
