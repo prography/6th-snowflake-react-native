5dd7df568b819e288ee342b6af9a9e6c5fc0f1f0
zwonH <a.circle.one@gmail.com>
2020-06-02 19:44:44 +0900

    feat: HomeMain 임시 loading

--- src/containers/home/main/Content.tsx
+++ src/containers/home/main/Content.tsx
@@ -1,81 +1,58 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ScrollView, Platform, Text } from 'react-native';
 import { d, BASE_URL } from '~/utils/constant';
 import HomeCardNoticePurple from '~/components/home/card/HomeCardNoticePurple';
 import HomeCardDefaultContentPurpleButton from '~/components/home/card/HomeCardDefaultContentPurpleButton';
 
 const Content = () => {
-  const [contentArray, setContentArray] = useState({});
-  const contentList = [
-    {
-      style: 'notice',
-      id: 1,
-      tag: [{ tag: 'Closed-beta' }, { tag: '많은_피드백_부탁드려요' }],
-      title: 'Welcome, PROGRAPHY!',
-      content: '건강한 사랑과 개방된 정보 공유,\n눈송이입니다.',
-      btnText: 'null',
-      link: 'null',
-    },
-    {
-      style: 'default',
-      id: 2,
-      tag: [{ tag: '제품' }, { tag: '반가워요' }],
-      title: '아무거나 써왔다면,',
-      content:
-        '소중한 내 몸에 닿는 제품, 적극적으로 알아보신 적 있으신가요? 기록 남는 것도 싫고, 또 자극적인 사이트가 불편하셨다면 눈송이의 제품 탭을 확인해 보세요. 언제 어디서나 당당하게 볼 수 있는 블라인드 기능도 잊지 마세요.',
-      btnText: '제품 보러 가기',
-      link: 'ProductStack',
-      //   'ProductStack', { screen: 'ProductInfo' } 가 들어가야 하는데 어떻게 넘겨주지
-    },
-  ];
+  const [contentArray, setContentArray] = useState(null);
 
   const _getWelcomCards = async () => {
     try {
       const response = await fetch(`${BASE_URL}/home/welcome-cards/`);
       const json = await response.json();
-      console.log(
-        '👻 welcome-cards success',
-        contentArray,
-        '😎태그를 위한 여정',
-        contentArray.map((card) => card.tag_txt.split(','))
-      );
+      console.log('👻 welcome-cards success', contentArray);
       setContentArray(json.results);
     } catch (error) {
       console.log('👻 welcome-cards error ', error);
     }
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     _getWelcomCards();
   }, []);
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      {contentArray.map((card) => {
-        switch (card.design_type) {
-          case 'default':
-            return (
-              <HomeCardDefaultContentPurpleButton
-                tag={card.tag_txt.split(',')}
-                title={card.title}
-                btnText={card.button_txt}
-                content={card.description}
-                link={card.button_src}
-              />
-            );
-          case 'notice':
-            return (
-              <HomeCardNoticePurple
-                tag={card.tag_txt.split(',')}
-                title={card.title}
-                content={card.description}
-              />
-            );
-          default:
-            return <Text>😭무언가 잘 못 되었음</Text>;
-        }
-      })}
+      {contentArray === null ? (
+        <Text>로딩 중</Text>
+      ) : (
+        contentArray.map((card) => {
+          switch (card.design_type) {
+            case 'default':
+              return (
+                <HomeCardDefaultContentPurpleButton
+                  tag={card.tag_txt.split(',')}
+                  title={card.title}
+                  btnText={card.button_txt}
+                  content={card.description}
+                  link={card.button_src}
+                />
+              );
+            case 'notice':
+              return (
+                <HomeCardNoticePurple
+                  tag={card.tag_txt.split(',')}
+                  title={card.title}
+                  content={card.description}
+                />
+              );
+            default:
+              return <Text>😭무언가 잘 못 되었음</Text>;
+          }
+        })
+      )}
     </ScrollView>
   );
 };
