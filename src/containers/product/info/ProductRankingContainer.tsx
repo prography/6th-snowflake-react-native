d00604e68e59af02041c61db7067da0fc9297d06
zwonH <a.circle.one@gmail.com>
2020-06-19 19:26:09 +0900

    test: 임의 버튼 눌러 카테고리 변경

--- src/containers/product/info/ProductRankingContainer.tsx
+++ src/containers/product/info/ProductRankingContainer.tsx
@@ -13,11 +13,23 @@ const Container = styled.View`
   flex: 1;
   align-items: flex-start;
 `;
+
+const CategoryWrapper = styled.View``;
+
+const Category = styled.TouchableOpacity`
+  width: ${d.px * 45}px;
+  height: ${d.px * 50}px;
+  background-color: blue;
+`;
+
 const ProductRankingContainer = () => {
   const [_rankingList, _setRankingList] = useState(null);
+  const [categoryParams, setCategoryParams] = useState('NORMAL');
   const _getRankingList = async () => {
     try {
-      const response = await fetch(`${BASE_URL}/products/condom/`);
+      const response = await fetch(
+        `${BASE_URL}/products/condom?category=${categoryParams}`
+      );
       const json = await response.json();
       _setRankingList(json.results);
       console.log('🧤Ranking List - success!', _rankingList);
@@ -28,11 +40,18 @@ const ProductRankingContainer = () => {
 
   useEffect(() => {
     _getRankingList();
-  }, []);
+  }, [categoryParams]);
   return (
     <>
       <ScrollView style={{ width: '100%', flex: 1 }}>
         <Container>
+          <CategoryWrapper>
+            <Category
+              onPress={() => {
+                setCategoryParams('SLIM');
+              }}
+            />
+          </CategoryWrapper>
           {_rankingList === null ? (
             <TextTitlePurpleRight title={'Loading...'} />
           ) : (
