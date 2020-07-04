0bb8d9d460f78a37cdfa5cfd197152badc3161d3
daheeahn <deg9810@gmail.com>
2020-07-04 16:32:22 +0900

    added sign in with apple capability

--- src/components/universal/button/AppleBtn.tsx
+++ src/components/universal/button/AppleBtn.tsx
@@ -1,57 +1,62 @@
 import * as React from 'react';
 import { useEffect } from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 import styled from 'styled-components/native';
 import { d, c } from '~/utils/constant';
 import appleAuth, {
-    AppleButton,
-    AppleAuthRequestOperation,
-    AppleAuthRequestScope,
-    AppleAuthCredentialState,
-  } from '@invertase/react-native-apple-authentication';
-  
+  AppleButton,
+  AppleAuthRequestOperation,
+  AppleAuthRequestScope,
+  AppleAuthCredentialState,
+} from '@invertase/react-native-apple-authentication';
+
 
 
 
 async function onAppleButtonPress() {
-    console.log('apple login 버튼');
-    // performs login request
-    const appleAuthRequestResponse = await appleAuth.performRequest({
-        requestedOperation: AppleAuthRequestOperation.LOGIN,
-        requestedScopes: [AppleAuthRequestScope.EMAIL, AppleAuthRequestScope.FULL_NAME],
-    });
+  console.log('apple login 버튼');
+  // performs login request
+  const appleAuthRequestResponse = await appleAuth.performRequest({
+    requestedOperation: AppleAuthRequestOperation.LOGIN,
+    requestedScopes: [AppleAuthRequestScope.EMAIL, AppleAuthRequestScope.FULL_NAME],
+  });
 
-    // get current authentication state for user
-    // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
-    const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+  console.log('🇬🇶🇬🇶🇬🇶🇬🇶🇬🇶🇬🇶1', appleAuthRequestResponse, appleAuthRequestResponse.user)
 
-    // use credentialState response to ensure the user is authenticated
-    if (credentialState === AppleAuthCredentialState.AUTHORIZED) {
-        // user is authenticated
-    }
+  // get current authentication state for user
+  // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+  const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+  console.log('🇬🇶🇬🇶🇬🇶🇬🇶🇬🇶🇬🇶2', credentialState)
+
+  // use credentialState response to ensure the user is authenticated
+  if (credentialState === AppleAuthCredentialState.AUTHORIZED) {
+    // user is authenticated
+    console.log('🇬🇶🇬🇶🇬🇶🇬🇶🇬🇶🇬🇶3', credentialState)
+
+  }
 }
 
 const AppleBtn = () => {
-    useEffect(() => {
-        // onCredentialRevoked returns a function that will remove the event listener. useEffect will call this function when the component unmounts
-        return appleAuth.onCredentialRevoked(async () => {
-          console.warn('If this function executes, User Credentials have been Revoked');
-        });
-      }, []); // passing in an empty array as the second argument ensures this is only ran once when component mounts initially.
-    
-    return (
-        <View>
-            <AppleButton
-                buttonStyle={AppleButton.Style.WHITE}
-                buttonType={AppleButton.Type.SIGN_IN}
-                style={{
-                  width: d.px * 160,
-                  height: d.px * 45,
-                }}
-                onPress={() => onAppleButtonPress()}
-              />
-        </View>
-    )
+  useEffect(() => {
+    // onCredentialRevoked returns a function that will remove the event listener. useEffect will call this function when the component unmounts
+    return appleAuth.onCredentialRevoked(async () => {
+      console.warn('If this function executes, User Credentials have been Revoked');
+    });
+  }, []); // passing in an empty array as the second argument ensures this is only ran once when component mounts initially.
+
+  return (
+    <View>
+      <AppleButton
+        buttonStyle={AppleButton.Style.WHITE}
+        buttonType={AppleButton.Type.SIGN_IN}
+        style={{
+          width: d.px * 160,
+          height: d.px * 45,
+        }}
+        onPress={() => onAppleButtonPress()}
+      />
+    </View>
+  )
 }
 
 export default AppleBtn;
