aaecc2f1df1ba25f52897bba91549539401de14c
zwonH <a.circle.one@gmail.com>
2020-07-04 12:57:18 +0900

    feat: email 중복 체크 api 적용(에러 메세지 아직), user data api 적용(로그인 시 닉네임)

--- src/containers/setting/MyProfile.tsx
+++ src/containers/setting/MyProfile.tsx
@@ -3,14 +3,18 @@ import { useEffect, useState } from 'react';
 import TextTitlePurpleRight from '~/components/universal/text/TextTitlePurpleRight';
 import { useSelector } from 'react-redux';
 import { d, BASE_URL, c, l } from '~/utils/constant';
+import styled from 'styled-components/native';
 import AsyncStorage, {
   useAsyncStorage,
 } from '@react-native-community/async-storage';
 import { AsyncAccessToken } from '~/utils/asyncStorage';
 
+const ProfileContainer = styled.View``;
+const UserName = styled.Text``;
 const MyProfile = () => {
   const _isLoggedin = useSelector((state) => state.authReducer.isLoggedin);
   const [token, setToken] = useState(null);
+  const [userInfoArray, setUserInfoArray] = useState([]);
 
   const _getUserInfo = async () => {
     try {
@@ -22,7 +26,7 @@ const MyProfile = () => {
     }
 
     try {
-      const response = await fetch(`${BASE_URL}/accounts`, {
+      const response = await fetch(`${BASE_URL}/accounts/`, {
         method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`,
@@ -31,6 +35,7 @@ const MyProfile = () => {
       const json = await response.json();
 
       console.log('🐹User info - success!', json);
+      setUserInfoArray(json);
     } catch (error) {
       console.log('🐹User info - error', error);
     }
@@ -43,7 +48,13 @@ const MyProfile = () => {
   return (
     <>
       {_isLoggedin ? (
-        <TextTitlePurpleRight title={'You are logged in ☀️'} />
+        <>
+          <ProfileContainer>
+            <TextTitlePurpleRight
+              title={userInfoArray.username + '님, 반가워요 ☀️'}
+            />
+          </ProfileContainer>
+        </>
       ) : (
         <TextTitlePurpleRight title={'Please join us! ☁️'} />
       )}
