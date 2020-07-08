f1e3225991265e90db8f50a171c4b7edbf31a984
Sangwon <jackoss@naver.com>
2020-07-08 23:53:50 +0900

    chore: kakao setting

--- ios/rnInit/AppDelegate.m
+++ ios/rnInit/AppDelegate.m
@@ -7,29 +7,29 @@
 
 #import <KakaoOpenSDK/KakaoOpenSDK.h>
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-                                      sourceApplication:(NSString *)sourceApplication
-                                              annotation:(id)annotation {
-    if ([KOSession isKakaoAccountLoginCallback:url]) {
-        return [KOSession handleOpenURL:url];
-    }
-
-    return false;
-}
-
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-                                                options:(NSDictionary<NSString *,id> *)options {
-    if ([KOSession isKakaoAccountLoginCallback:url]) {
-        return [KOSession handleOpenURL:url];
-    }
-
-    return false;
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    [KOSession handleDidBecomeActive];
-}
+//- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+//                                      sourceApplication:(NSString *)sourceApplication
+//                                              annotation:(id)annotation {
+//    if ([KOSession isKakaoAccountLoginCallback:url]) {
+//        return [KOSession handleOpenURL:url];
+//    }
+//
+//    return false;
+//}
+//
+//- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+//                                                options:(NSDictionary<NSString *,id> *)options {
+//    if ([KOSession isKakaoAccountLoginCallback:url]) {
+//        return [KOSession handleOpenURL:url];
+//    }
+//
+//    return false;
+//}
+//
+//- (void)applicationDidBecomeActive:(UIApplication *)application
+//{
+//    [KOSession handleDidBecomeActive];
+//}
 
 
 
