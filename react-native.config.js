193b043781f620e590e393647cd4e377f21f6c9f
daheeahn <deg9810@gmail.com>
2020-07-24 22:16:20 +0900

    bugfix: codepush 컴포넌트로 안감싸줘서 안먹혔던거~ 이제 코드푸시 됨 & 버전 업그레이드하고 autolink 먹힘 add: 여러 명령어 단축어로 만들었음 (package.json)

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   assets: ["./src/assets/fonts"],
   dependencies: {
-    "react-native-code-push": {
-      platforms: {
-        android: null, // disable Android platform, other platforms will still autolink if provided
-        ios: null,
-      },
-    },
     "@react-native-seoul/kakao-login": {
       platforms: {
         android: null,
