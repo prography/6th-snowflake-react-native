a1c924bbce6a2ade02ea63bbe959ff54b218d7ae
daheeahn <deg9810@gmail.com>
2020-09-14 23:40:42 +0900

    test chore: test용 redux saga 코드 작성

---
+++ src/components/_for_test/CounterBox.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { Text } from 'react-native';
+import styled from 'styled-components/native';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { RootState } from '~/store/modules';
+import MarginNarrow from '../universal/margin/MarginNarrow';
+import { increaseAsync, decreaseAsync } from '~/store/modules/counter-test';
+import { getUserInfo } from '~/store/saga/join/userInfo';
+
+interface Props {
+
+}
+
+const Container = styled.View`
+  /* flex-direction: row; */
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: lightgreen;
+  border-width: 1px;
+  border-color: green;
+  padding: 10px;
+`;
+
+export default ({ }: Props) => {
+  const count = useSelector((state: RootState) => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <Container>
+      <Text>Count: {count}</Text>
+
+      <MarginNarrow />
+
+      <Button onPress={() => dispatch(increaseAsync())}>
+        <Text>+ 1</Text>
+      </Button>
+      <MarginNarrow />
+      <Button onPress={() => dispatch(decreaseAsync())}>
+        <Text>- 1</Text>
+      </Button>
+      <Button onPress={() => dispatch(getUserInfo())}>
+        <Text>Get User Info</Text>
+      </Button>
+      <MarginNarrow />
+    </Container>
+  );
+};
---
+++ src/store/modules/counter-test/index.ts
@@ -0,0 +1,36 @@
+/******** For Test ********/
+
+import { llog2 } from "~/utils/functions";
+
+// 액션 타입
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+export const INCREASE_ASYNC = "INCREASE_ASYNC";
+export const DECREASE_ASYNC = "DECREASE_ASYNC";
+
+// 액션 생성 함수
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
+
+export type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+const CounterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREASE:
+      return { ...state, count: state.count + 1 };
+    case DECREASE:
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+export default CounterReducer;
