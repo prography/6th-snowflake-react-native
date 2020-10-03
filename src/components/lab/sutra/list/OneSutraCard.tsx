80ce0611e32f6c0599d49c35985e5aeaa3ff141b
zwonH <a.circle.one@gmail.com>
2020-10-03 18:33:41 +0900

    feat: OneSutraCard 디자인 적용

--- src/components/lab/sutra/list/OneSutraCard.tsx
+++ src/components/lab/sutra/list/OneSutraCard.tsx
@@ -1,23 +1,72 @@
 import * as React from 'react';
+import { Text } from 'react-native';
 import { useState } from 'react';
 import styled from 'styled-components/native';
 import { d, l, c } from '~/utils/constant';
 import MarginWide from '~/components/universal/margin/MarginWide';
+import MarginNarrow from '~/components/universal/margin/MarginNarrow';
 
 const Container = styled.View`
   margin-right: ${l.mR}px;
   margin-left: ${l.mR}px;
 `;
-const ImageContainer = styled.View``;
+const TopArea = styled.View`
+  flex-direction: row;
+  width: 100%;
+`;
+const ImageContainer = styled.View`
+  margin-right: ${d.px * 15}px;
+  flex: 1;
+`;
 const BookmarkImage = styled.Image``;
-const SutraImage = styled.Image``;
-const SelectionContainer = styled.View``;
+const SutraImage = styled.Image`
+  width: 100%;
+  height: ${d.px * 120}px;
+`;
+const SelectionContainer = styled.View`
+  flex: 1;
+`;
 const GoodBadScore = styled.View``;
 const PurpleSkyScore = styled.View``;
-const GoodOrBad = styled.View``;
-const Good = styled.TouchableOpacity``;
-const Bad = styled.TouchableOpacity``;
-const NotYet = styled.TouchableOpacity``;
+const GoodOrBadButtonContainer = styled.View`
+  width: 100%;
+  height: ${d.px * 60}px;
+  flex-direction: row;
+  border-color: ${c.darkGray};
+  border-width: ${d.px * 0.5}px;
+  border-style: solid;
+  margin-bottom: ${d.px * 20}px;
+`;
+const GoodButton = styled.TouchableOpacity`
+  flex: 1;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: ${c.mint};
+`;
+const BadButton = styled.TouchableOpacity`
+  flex: 1;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+
+const GoodBadText = styled.Text`
+  font-family: Jost-Medium;
+  font-size: ${d.px * 15}px;
+  color: ${c.black};
+`;
+const NotYet = styled.TouchableOpacity`
+  width: 100%;
+  height: ${d.px * 40}px;
+
+  justify-content: center;
+  align-items: center;
+  border-color: ${c.darkGray};
+  border-width: ${d.px * 0.5}px;
+  border-style: solid;
+`;
 const SutraTitle = styled.Text`
   font-family: Jost-Semi;
   font-size: ${d.px * 17}px;
@@ -45,25 +94,39 @@ const OneSutraCard = () => {
   return (
     <>
       <Container>
-        <ImageContainer>
-          <BookmarkImage />
-          <SutraImage />
-        </ImageContainer>
-        {selected ? (
-          <SelectionContainer>
-            <GoodBadScore />
-            <PurpleSkyScore />
-          </SelectionContainer>
-        ) : (
-          <SelectionContainer>
-            <GoodOrBad>
-              <Good />
-              <Bad />
-            </GoodOrBad>
-            <NotYet />
-          </SelectionContainer>
-        )}
+        <TopArea>
+          <ImageContainer>
+            <BookmarkImage />
+            <SutraImage
+              style={{ resizeMode: 'cover' }}
+              source={require('~/img/sample/sutraSample.jpg')}
+            />
+          </ImageContainer>
+          <Text />
+          {selected ? (
+            <SelectionContainer>
+              <GoodBadScore />
+              <PurpleSkyScore />
+            </SelectionContainer>
+          ) : (
+            <SelectionContainer>
+              <GoodOrBadButtonContainer>
+                <GoodButton>
+                  <GoodBadText>추천</GoodBadText>
+                </GoodButton>
+                <BadButton>
+                  <GoodBadText>비추</GoodBadText>
+                </BadButton>
+              </GoodOrBadButtonContainer>
+              <NotYet>
+                <GoodBadText>안 해 봤어요</GoodBadText>
+              </NotYet>
+            </SelectionContainer>
+          )}
+        </TopArea>
+        <MarginNarrow />
         <SutraTitle>체위 한글 이름</SutraTitle>
+        <MarginNarrow />
         <CommentWrapper>
           <CommentUsername>닉네임</CommentUsername>
           <CommentText>실시간 댓글 우와우</CommentText>
