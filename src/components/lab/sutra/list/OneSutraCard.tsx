c6a764c54bdb45b4a731c806084b996ad14c963a
zwonH <a.circle.one@gmail.com>
2020-10-03 22:57:21 +0900

    feat: OneSutraCard 북마크 및 디자인 마무리

--- src/components/lab/sutra/list/OneSutraCard.tsx
+++ src/components/lab/sutra/list/OneSutraCard.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components/native';
 import { d, l, c } from '~/utils/constant';
 import MarginWide from '~/components/universal/margin/MarginWide';
 import MarginNarrow from '~/components/universal/margin/MarginNarrow';
+import ProductInfoSpecific from '~/containers/product/info/ProductInfoSpecific';
 
 const Container = styled.View`
   margin-right: ${l.mR}px;
@@ -18,7 +19,19 @@ const ImageContainer = styled.View`
   margin-right: ${d.px * 15}px;
   flex: 1;
 `;
-const BookmarkImage = styled.Image``;
+const BookmarkContainer = styled.TouchableOpacity`
+  position: absolute;
+  width: ${d.px * 40}px;
+  height: ${d.px * 45}px;
+  top: ${-d.px * 1}px;
+  margin-left: ${d.px * 10}px;
+
+  align-items: center;
+`;
+const BookmarkImage = styled.Image`
+  width: ${d.px * 33}px;
+  height: ${d.px * 40}px;
+`;
 const SutraImage = styled.Image`
   width: 100%;
   height: ${d.px * 120}px;
@@ -26,8 +39,71 @@ const SutraImage = styled.Image`
 const SelectionContainer = styled.View`
   flex: 1;
 `;
-const GoodBadScore = styled.View``;
-const PurpleSkyScore = styled.View``;
+const GoodScoreContainer = styled.View`
+  width: 100%;
+  height: ${d.px * 60}px;
+  flex-direction: row;
+  border-color: ${c.darkGray};
+  border-width: ${d.px * 0.5}px;
+  border-style: solid;
+  margin-bottom: ${d.px * 13}px;
+  align-items: center;
+  justify-content: center;
+`;
+const GoodScore = styled.View`
+  width: ${(props) => props.score}%;
+  height: 100%;
+  background-color: ${c.mint};
+  position: absolute;
+  left: 0;
+`;
+const GoodScoreText = styled.Text`
+  font-family: Jost-Medium;
+  font-size: ${d.px * 17}px;
+  color: ${c.black};
+`;
+const PurpleSkyScoreContainer = styled.View`
+  flex-direction: column;
+  width: 100%;
+`;
+const PurpleScoreContainer = styled.View`
+  flex-direction: column;
+`;
+const PurpleHead = styled.Image`
+  height: ${d.px * 23}px;
+  width: ${d.px * 35}px;
+  margin-right: ${d.px * 5}px;
+`;
+const PurpleScoreWrapper = styled.View`
+  width: 100%;
+  position: absolute;
+  height: ${d.px * 23}px;
+  justify-content: center;
+`;
+const PurpleScore = styled.View`
+  height: ${d.px * 5}px;
+  width: ${(props) => props.score}%;
+  background-color: ${c.purple};
+`;
+const SkyScoreContainer = styled.View`
+  flex-direction: row;
+`;
+const SkyHead = styled.Image`
+  height: ${d.px * 23}px;
+  width: ${d.px * 35}px;
+  margin-right: ${d.px * 5}px;
+`;
+const SkyScoreWrapper = styled.View`
+  width: 100%;
+  position: absolute;
+  height: ${d.px * 23}px;
+  justify-content: center;
+`;
+const SkyScore = styled.View`
+  height: ${d.px * 5}px;
+  width: ${(props) => props.score}%;
+  background-color: ${c.purple};
+`;
 const GoodOrBadButtonContainer = styled.View`
   width: 100%;
   height: ${d.px * 60}px;
@@ -43,6 +119,9 @@ const GoodButton = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
   background-color: ${c.mint};
+  border-color: ${c.darkGray};
+  border-right-width: ${d.px * 0.5}px;
+  border-style: solid;
 `;
 const BadButton = styled.TouchableOpacity`
   flex: 1;
@@ -90,35 +169,77 @@ const CommentText = styled.Text`
 
 // 하나짜리 컴포넌트! SutraCardsList에서 데이터 받아와서 list로...!
 const OneSutraCard = () => {
+  const [bookmarked, setBookmarked] = useState(false);
   const [selected, setSelected] = useState(false);
   return (
     <>
       <Container>
         <TopArea>
           <ImageContainer>
-            <BookmarkImage />
             <SutraImage
               style={{ resizeMode: 'cover' }}
               source={require('~/img/sample/sutraSample.jpg')}
             />
+            <BookmarkContainer activeOpacity={1}>
+              {/* 찜했으면 보라색으로, 찜 안 한 건 하얀색으로 */}
+              {bookmarked ? (
+                <BookmarkImage
+                  style={{ resizeMode: 'contain' }}
+                  source={require('~/img/icon/iconBookmarkSelected.png')}
+                />
+              ) : (
+                <BookmarkImage
+                  style={{ resizeMode: 'contain' }}
+                  source={require('~/img/icon/iconBookmarkUnselected.png')}
+                />
+              )}
+            </BookmarkContainer>
           </ImageContainer>
           <Text />
+          {/* 내가 추천/비추/안해봤 중에 하나를 누르면 
+            selected가 true가 되면서,
+            통계를 볼 수 있게끔
+          */}
           {selected ? (
             <SelectionContainer>
-              <GoodBadScore />
-              <PurpleSkyScore />
+              <GoodScoreContainer>
+                {/* score들의 숫자는 임의로 넣은 점수... 받아온 점수가 들어가면 됨! */}
+                <GoodScore score={74.5} />
+                <GoodScoreText>추천 74.5%</GoodScoreText>
+              </GoodScoreContainer>
+
+              <PurpleSkyScoreContainer>
+                <PurpleScoreContainer>
+                  <PurpleScoreWrapper>
+                    <PurpleScore score={84.6} />
+                  </PurpleScoreWrapper>
+                  <PurpleHead
+                    style={{ resizeMode: 'contain' }}
+                    source={require('~/img/sample/purpleCharacHeadSample.png')}
+                  />
+                </PurpleScoreContainer>
+                <SkyScoreContainer>
+                  <SkyScoreWrapper>
+                    <SkyScore score={68.3} />
+                  </SkyScoreWrapper>
+                  <SkyHead
+                    style={{ resizeMode: 'contain' }}
+                    source={require('~/img/sample/skyCharacHeadSample.png')}
+                  />
+                </SkyScoreContainer>
+              </PurpleSkyScoreContainer>
             </SelectionContainer>
           ) : (
             <SelectionContainer>
               <GoodOrBadButtonContainer>
-                <GoodButton>
+                <GoodButton activeOpacity={1}>
                   <GoodBadText>추천</GoodBadText>
                 </GoodButton>
-                <BadButton>
+                <BadButton activeOpacity={1}>
                   <GoodBadText>비추</GoodBadText>
                 </BadButton>
               </GoodOrBadButtonContainer>
-              <NotYet>
+              <NotYet activeOpacity={1}>
                 <GoodBadText>안 해 봤어요</GoodBadText>
               </NotYet>
             </SelectionContainer>
