e17b82e418e856494100acad3bb7630d59742f68
daheeahn <deg9810@gmail.com>
2020-12-14 03:46:10 +0900

    주석 추가

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ import App from "./App";
 import { name as appName } from "./app.json";
 import codePush from "react-native-code-push";
 
+// console.disableYellowBox = true; // 노란 경고창 보기 싫을 때
+
 AppRegistry.registerComponent(appName, () => codePush(App));
